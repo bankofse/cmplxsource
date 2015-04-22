d03345eb081f094a2e896605fc83e1b3667d6c7c
Michael Timbrook <timbrook480@gmail.com>
2015-04-22 00:20:34 -0400

    Auth yo!

--- web/js/stores/UserStore.js
+++ web/js/stores/UserStore.js
@@ -1,18 +1,61 @@
 "use strict";
 
 var Fluxxor = require('fluxxor'),
-    consts  = require('../constants/const');
+    consts  = require('../constants/const'),
+    rest    = require('rest'),
+    mime    = require('rest/interceptor/mime'),
+    defaultRequest = require('rest/interceptor/defaultRequest')
+;
+
+const AUTHKEY = "auth-token";
 
 var UserStore  = Fluxxor.createStore({
-  initialize: function() {
 
+  initialize: function() {
+    this.bindActions(
+      consts.LOGIN_USER, this.loginUser,
+      consts.LOGOUT_USER, this.logoutUser
+    ); 
     this.loggedin = false;
+
+    rest.wrap(defaultRequest, { 
+      method: "GET"
+    })
+    ({
+      path: "https://cmplx.in/user",
+      headers: { 'X-Requested-With': 'rest.js' }
+    })
+    .then(function (response) {
+      console.log(response);
+    }.bind(this))
+    .catch(function (err) {
+      console.log(err);
+    });
 
   },
 
   loginUser: function(payload) {
-    this.loggedin = true;
-    this.emit("change");
+    console.log("Loggin in user", payload);
+    rest.wrap(mime, { mime: 'application/json' })({
+      path: "https://cmplx.in/auth",
+      method: "POST",
+      entity: payload
+    })
+    .then(function (response) {
+      switch(response.status.code) {
+
+        case 200:
+          this.loggedin = true;
+          localStorage.setItem(AUTHKEY, response.entity.token);
+          this.emit("change");
+          break;
+
+        default:
+          console.log("Not logged in")
+
+      }
+    }.bind(this));
+
   },
 
   logoutUser: function() {
@@ -23,6 +66,7 @@ var UserStore  = Fluxxor.createStore({
   getState: function() {
     return { loggedin : this.loggedin };
   }
+
 });
 
 module.exports = UserStore;
