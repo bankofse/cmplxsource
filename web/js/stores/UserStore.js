bb2e2b361e5cff95a21af2a70f1a6a510b99692e
Michael Timbrook <timbrook480@gmail.com>
2015-04-21 22:19:58 -0400

    Whoops I dfuped

--- web/js/stores/UserStore.js
+++ web/js/stores/UserStore.js
@@ -5,53 +5,23 @@ var Fluxxor = require('fluxxor'),
 
 var UserStore  = Fluxxor.createStore({
   initialize: function() {
-<<<<<<< Updated upstream
-    this.loggedin = false;
-    this.jwt = null;
-=======
-    let authKey;
-    let hasKey = localStorage.getItem("auth-token");
-
-    if (hasKey) {
-      authKey = hasKey;
-
-      $.get('https://cmplx.in/auth', function () {
-        console.log(arguments);
-      });
-
-    } else {
-      this.loggedin = false;
-    }
 
     this.loggedin = false;
->>>>>>> Stashed changes
-    this.bindActions(
-        consts.LOGIN_USER, this.loginUser,
-        consts.LOGOUT_USER, this.logoutUser
-    );
 
   },
 
   loginUser: function(payload) {
     this.loggedin = true;
-<<<<<<< Updated upstream
-    this.jwt = payload.jwt;
     this.emit("change");
   },
 
   logoutUser: function() {
     this.loggedin = false;
-    this.jwt = null;
-=======
-
-    
-    
->>>>>>> Stashed changes
     this.emit("change");
   },
 
   getState: function() {
-    return { loggedin : this.loggedin, jwt: this.jwt, jwt_decode: this.jwt_decode };
+    return { loggedin : this.loggedin };
   }
 });
 
