a612df4ebdc0bd84e87f394202096749b20e8a8e
Steven Kroh <sk.kroh@gmail.com>
2015-05-07 01:58:44 -0400

    Adding everything back

---
+++ config/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+	redis : {
+		port: 6379,
+		host: "localhost"
+	},
+	disque: {
+		port: 7711,
+		host: "localhost"
+	},
+	postgrest: {
+		host: "http://localhost:3001"
+	}
+}
---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+HOST="localhost:3000";
+DB="http://localhost:3001";
+echo "Creating User Accounts";
+
+NUMUSERS=$(cat ./data.json | jsawk 'return this.accounts.length - 1');
+
+for i in $(seq 0 $NUMUSERS); do 
+  PAYLOAD=$(cat ./data.json | jsawk "return this.accounts[$i]");
+  cat ./data.json | jsawk "return this.accounts[$i].user";
+  curl -H "Content-Type:application/json" -XPOST  $HOST/auth/create -d "$PAYLOAD";
+  #echo -ne "\r $?| Done  $(jot -b ' ' -s '' $COLUMNS - 10)\n";
+done
+
+NUMACCOUNTS=$(curl -s -H "Content-Type:application/json" $DB/accounts | jsawk -a 'return this.length');
+echo "Total monitary accounts in system is $NUMACCOUNTS";
+echo "Give each how much?";
+read AMOUNT;
+for i in $(seq 0 $NUMACCOUNTS); do 
+	TRANS=$(cat data.json| jsawk "this.transaction.amount = $AMOUNT; this.transaction.account=$i; return this.transaction");
+	curl -H "Content-Type:application/json" -XPOST  $DB/transactions -d "$TRANS";
+done
+
+echo "Done";
